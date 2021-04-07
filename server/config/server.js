73fc2c6c22690b62fa4192346702748004f91c69
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-04-07 15:29:41 +0200

    adding strapi project #2

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3ffe4ea89facdc7426d257f109eaa81a'),
+    },
+  },
+});
