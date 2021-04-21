a5c0c4459e814b876934efe39de06f48012d147b
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-04-21 10:46:21 +0200

    adding glasses as second step on page #26 #12

--- client/components/Glasses.js
+++ client/components/Glasses.js
@@ -1,52 +1,48 @@
 import styles from "./Glasses.module.css";
 import { useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/router";
-
 
 export default function Glasses({ glasses, onSubmit }) {
 
-    const [checkedGlass, setCheckedGlass] = useState(null);
-    const router = useRouter();
+    const [checkedGlass, setCheckedGlass] = useState();
 
     const addGlass = (e) => {
         e.preventDefault();
-
         const data = {
             glass: checkedGlass,
         }
-
         e.target.reset();
         onSubmit(data);
-
-        router.push("/bar");
-    }
-
-    const checkGlass = (e) => {
-        console.log(e.target.value);
-        setCheckedGlass(e.target.value)
     }
     
     return (
-        <form onSubmit={(e) => addGlass(e)} className={styles.counter}>
-        <input type="submit"  className={styles.button} value={checkedGlass}/> 
+        <form onSubmit={(e) => addGlass(e)} className={styles.content}>
+        
         <div className={styles.glassOverview}> 
             {glasses.map((glass) => (
                 <div key={glass.id}  className={styles.glassButton}>
-                    <input onChange={(e) => checkGlass(e)} type="radio" id={glass.id} name="glass" value={glass.name} className={styles.radioButton}/>
+
                     <div className={styles.glassImage}>
-                    <label htmlFor={glass.name} className={styles.label}>{glass.name}</label>
-                    
                         <Image 
                             src={process.env.STRAPI_URL + glass.image.url} 
                             width={glass.image.width /1.5} 
-                            height={glass.image.height /1.5}/>
-                            
-                    </div>     
+                            height={glass.image.height /1.5}/>           
+                    </div>   
+
+                    <input onChange={(e) => setCheckedGlass(e.target.value)} 
+                            type="radio" 
+                            id={glass.id} 
+                            name="glass" 
+                            value={glass.name} 
+                            className={styles.radioButton}/>
+                    <label htmlFor={glass.name} 
+                            className={styles.label}>
+                        {glass.name}</label>
                 </div>            
             ))}     
         </div>  
-            
+
+        <input type="submit"  className={styles.button} value="Beverages"/>     
         </form>
     )
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,38 @@
 import Welcome from "../components/Welcome";
-import Link from "next/link";
-import { useState } from "react";
+import Navigation from "../components/Navigation";
+import Glasses from "../components/Glasses";
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
 
-export default function Home() {
+export default function Home({ glasses }) {
 
-  // const [currentStep, setCurrentStep] = useState("home");
+  const [currentStep, setCurrentStep] = useState("");
+
+  const handleSubmit = async (data) => {
+    const check = glasses.filter((glass) => glass.name === data.glass); 
+    data.glass = check[0].id; 
+
+    await fetch(`${process.env.STRAPI_URL}/cocktails/`,
+    {
+      method: "POST",
+      body: JSON.stringify(data), 
+      headers: {
+        "Content-type": "application/json; charset=UTF-8", 
+      },
+    });
+  };
+
+  if (currentStep === "first") {
+    return (
+      <Navigation>
+        <div className={styles.content}>
+          <p className={styles.description}>Choose a glass</p>
+        </div>
+
+        <Glasses glasses={glasses} onSubmit={handleSubmit}/>
+      </Navigation>
+    )
+  }
 
 
   return (
@@ -15,8 +42,19 @@ export default function Home() {
           Welcome to our online Cocktail Bar where you can mix and match your favourite drinks and toost to a better future!
         </p>
           
-        <button className={styles.button}>Let's start shaking</button>
+        <button onClick={(e) => setCurrentStep(e.target.name)} name="first" className={styles.button}>Let's start shaking</button>
       </div>
     </Welcome>      
   )
 }
+
+export async function getStaticProps () {
+  const response = await fetch(`${process.env.STRAPI_URL}/glasses`);
+  const glasses = await response.json();
+
+  return {
+    props: {
+      glasses,
+    },
+  };
+};
