8de5ba0da0f21fbdbdf23ba2e5afc0593a7fd60a
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-04-23 13:37:47 +0200

    implementing contentful #31

--- server/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '5adc03922ab53ad8d97d2a400a1f5c8c'),
-    },
-  },
-});
