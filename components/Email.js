55fbd5c0e7633ab66f5319c8e96f68308d56ea49
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-04-27 16:01:22 +0200

    create seperate component email #45

---
+++ components/Email.js
@@ -0,0 +1,31 @@
+import styles from "./Email.module.css";
+
+export default function Email ({ onSubmit }) {
+
+    const handleSubmitEmail = (e) => {
+        e.preventDefault(); 
+
+        console.log(e.target.email.value);
+
+        const data = {
+            email: e.target.email.value,
+        }
+
+        onSubmit(data);
+    }
+
+    return (
+        <div className={styles.container}>
+        <h2 className={styles.title}>Your cocktail is being made</h2>
+
+        <form className={styles.mail} onSubmit={(e) => handleSubmitEmail(e)}>
+
+            <label className={styles.label}> Your friend's email
+                <input className={styles.input} type="email" name="email" required placeholder="name@email.com"/>
+            </label>
+
+            <input className={styles.submitButton} type="submit" value="Send"/>
+        </form>
+    </div>
+    )
+}
--- pages/success/index.js
+++ pages/success/index.js
@@ -1,23 +1,16 @@
 import Layout from "../../components/Layout";
+import Email from "../../components/Email";
 import styles from "./Success.module.css";
 
-export default function Success({ }) {
+export default function Success({ onSubmit }) {
+
+    const handleSubmitEmail = data => {
+       console.log(data);
+    }
 
     return (
         <Layout>
-            <div className={styles.container}>
-                <h2 className={styles.title}>Your cocktail is being made</h2>
-
-                <form className={styles.mail}>
-
-                    <label className={styles.label}> Your friend's email
-                        <input className={styles.input} type="email" required placeholder="name@email.com"/>
-                    </label>
-
-                    <input className={styles.submitButton} type="submit" value="Send"/>
-
-                </form>
-            </div>
+            <Email onSubmit={handleSubmitEmail}/>
         </Layout>
     )
 }
