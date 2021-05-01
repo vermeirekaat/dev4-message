edbfb7d81722a8683a71f1854518a274fea9501e
Kaat Vermeire <kaat.vermeire@student.howest.be>
2021-05-01 20:59:53 +0200

    finetune animation on detail page #65

--- components/Final.js
+++ components/Final.js
@@ -4,23 +4,21 @@ import { motion } from "framer-motion";
 
 export default function Final ({ cocktail }) {
     return (
-        <div className={styles.overview}>
-            <div className={styles.input}>
-                        <motion.div className={styles.image}
-                                    initial={{ y: 0, x: "15vw", scale: 1, opacity: 1 }}
-                                    animate={{ y: 0, x: 0, scale: 1, opacity: 1}}
-                                    transition={{ delay: .7, ease:"easeOut", duration: 3}}>
-                            <Image 
-                                src={"https:" + cocktail.fields.glass.fields.image.fields.file.url} 
-                                width={cocktail.fields.glass.fields.image.fields.file.details.image.width} 
-                                height={cocktail.fields.glass.fields.image.fields.file.details.image.height}
-                            />
-                        </motion.div>
-            </div>
+        <div className={styles.container}>
+            <motion.div className={styles.image}
+                        initial={{ opacity: 1, y: 0, x: 0 }}
+                        animate={{ opacity: 1, y: 0, x: "-20vw",}}
+                        transition={{ delay: .7, ease:"easeOut", duration: 3}}>
+                    <Image 
+                        src={"https:" + cocktail.fields.glass.fields.image.fields.file.url} 
+                        width={cocktail.fields.glass.fields.image.fields.file.details.image.width} 
+                        height={cocktail.fields.glass.fields.image.fields.file.details.image.height}
+                        />
+            </motion.div>
 
             <motion.div className={styles.content}
-                        initial={{ x: "15vh", opacity: 0}}
-                        animate={{ x: 0, opacity: 1}}
+                        initial={{ x: 0, opacity: 0}}
+                        animate={{ x: "-35vw", opacity: 1}}
                         transition={{ duration: 4 }}>
                 <p className={styles.item}>To: {cocktail.fields.receiver}</p>
                 <div className={styles.messageContainer}>
